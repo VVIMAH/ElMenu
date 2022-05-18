0d5cab7d576b5b58533378cdbac11c92f2fb93a2
IVainqueur <ishimvainqueur@gmail.com>
2022-05-18 08:53:42 +0200

    Part of the landing page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import LandingPage from './components/landing/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element = {<LandingPage />} ></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
