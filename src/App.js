f67540e68550eadbaff826f50f02f6cd560d76da
Vanex-h <vanessahirwa5@gmail.com>
2022-05-18 11:05:09 +0200

    Signup is kinda done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,49 @@ function App() {
   return (
     <div className="App">
       
+    <div className='one'>
+      
+
+<img src={require('../src/Group 5.png')} alt="" />
+      
+      </div>
+    
+    <div className='two'>
+    <h1>Sign Up</h1>
+    
+    <form action="#" method='POST'>
+      <div className='Field'>
+        <label >FIRST NAME</label><br />
+        <input type="text" name='firstname' placeholder='First Name' /><br />
+      </div>
+      <div className='Field'>
+        <label >LAST NAME</label><br />
+        <input type="text" name='lastname' placeholder='Last Name' /><br />
+      </div>
+      <div className='Field'>
+        <label >PHONE</label><br />
+        <input type="number" name='phone'  /><br />
+      </div>
+      <div className='Field'>
+        <label >EMAIL </label><br />
+        <input type="email" name='email' placeholder='Email Address' /><br />
+      </div>
+      <div className='Field'>
+        <label >PASSWORD</label><br />
+        <input type="password" name='password' placeholder='Password' /><br />
+      </div>
+     
+<div className='Field'>
+      <button type="submit" className='sub'>Sign up</button>
+  </div>
+
+    <div className='a'>
+      Already have an account?  
+      <a href="https://fonts.google.com/specimen/Raleway?query=raleway">Login</a>
+      </div>
+    </form>
+   
+    </div>
     </div>
   );
 }
